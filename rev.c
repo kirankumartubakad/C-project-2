350512cba513dd17ac7b7da3d0aa4b520ed1ede8
ankit100391 <92242141+ankit100391@users.noreply.github.com>
2023-03-02 22:12:33 +0530

    Update rev.c

--- rev.c
+++ rev.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-reverse() {
+reverse()
+{
 
   int n, reverse = 0, remainder;
 
@@ -15,5 +16,5 @@ reverse() {
 
   printf("Reversed number = %d", reverse);
 
-  return 0;
+  \\return 0;
 }
